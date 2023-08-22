65f16ddea57ecc8c082c73ac63a940ee0a99eb38
Mehedi529 <mehedihasan49265@gmail.com>
2023-08-23 01:06:17 +0600

    javascript es6 map

---
+++ assignmentProblem5.js
@@ -0,0 +1,8 @@
+function doubleNumbers(numbers) {
+    const doubledArr = numbers.map(num => num * 2);
+    return doubledArr;
+  }
+  
+  const numbers = [1, 2, 3, 4, 5];
+  console.log(doubleNumbers(numbers));
+  
